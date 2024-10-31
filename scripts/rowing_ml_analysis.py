2fddfc4f66b405d6e7b29fb47fba5433177a04df
pjr2359 <123979672+pjr2359@users.noreply.github.com>
2024-10-31 08:42:14 -0400

    started using venv instead of conda because of weird tensorflow issues

--- scripts/rowing_ml_analysis.py
+++ scripts/rowing_ml_analysis.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import os
